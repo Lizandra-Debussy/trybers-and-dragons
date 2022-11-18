4ba19bc060df237d15613618f22b7b18431b7a01
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-11-18 20:36:20 -0300

    Classe Archetype criada

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,34 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() { 
+    return this._name; 
+  }
+
+  get special() { 
+    return this._special; 
+  }
+
+  get cost() { 
+    return this._cost; 
+  }
+
+  static createdArchetypeInstances(instances: number) {
+    if (!instances) throw new Error('Not implemented');
+    return instances;
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
